4932c0e66bc244a90e530dfb05a1bd5b32c0dd59
Jodi Warren <jodi@jodiwarren.com>
2020-06-29 06:47:34 +0100

    Updates

---
+++ precache-manifest.5d0b7f23ab6e86d6f37476afd5def89a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53fe4179c216b063ceb603aecd878cd9",
+    "url": "/babysimulator/index.html"
+  },
+  {
+    "revision": "e8d4ba966b4b46319963",
+    "url": "/babysimulator/static/css/main.a032c357.chunk.css"
+  },
+  {
+    "revision": "a76c244a43ed06920b66",
+    "url": "/babysimulator/static/js/2.21bb8bcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/babysimulator/static/js/2.21bb8bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d4ba966b4b46319963",
+    "url": "/babysimulator/static/js/main.34f89385.chunk.js"
+  },
+  {
+    "revision": "b8bfd9752bb33925aa3c",
+    "url": "/babysimulator/static/js/runtime-main.9de517cf.js"
+  }
+]);
